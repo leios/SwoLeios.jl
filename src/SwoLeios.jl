7d4fc32836b78e0d637fc976ebfaad3beaacf6ef
James Schloss <jrs.schloss@gmail.com>
2021-03-27 16:02:30 -0400

    fixing bug in select_set\!(...)

--- src/SwoLeios.jl
+++ src/SwoLeios.jl
@@ -1,6 +1,6 @@
 module SwoLeios
 
-export next_set
+export cycle_set!, next_set, output_set
 
 using DataFrames
 using CSV
@@ -8,10 +8,26 @@ using CSV
 include("selection.jl")
 include("update.jl")
 
+function output_set(superset)
+    (set, reps) = next_set(superset)
+    output_set(set, reps)
+end
+
+function output_set(set, reps)
+    set_str = string(reps)*'\t'*string(set[:exercise])
+    return set_str
+end
+
 function next_set(superset)
+    set = copy(superset[1][1][1,:])
+    reps = select_reps(set)
+    return (set, reps)
+end
+
+function cycle_set!(superset)
     set = select_set!(superset)
     reps = select_reps(set)
-    return string(reps)*'\t'*string(set[:exercise])
+    return output_set(set, reps)
 end
 
 end # module
--- src/selection.jl
+++ src/selection.jl
@@ -12,18 +12,18 @@ function convert_to_float(value::String)
 end
 
 function convert_to_time(value::Float64)
-    return string(floor(Int, value))*":"*string(convert(Int,(value%1)*60))
+    return string(floor(Int, value))*":"*lpad(string(floor(Int,(value%1)*60)), 2, "0")
 end
 
-function select_reps(exercise::DataFrameRow)
-    difficulty = exercise["difficulty"]
-    reps_0 = convert_to_float(exercise["reps_0"])
-    reps_10 = convert_to_float(exercise["reps_10"])
+function select_reps(exercise)
+    difficulty = exercise[:difficulty]
+    reps_0 = convert_to_float(exercise[:reps_0])
+    reps_10 = convert_to_float(exercise[:reps_10])
 
     value = reps_0 + (reps_10 - reps_0)*difficulty/10
 
     # determining whether set is in reps or duration
-    colon_loc = findfirst(":", exercise["reps_0"])
+    colon_loc = findfirst(":", exercise[:reps_0])
 
     if typeof(colon_loc) != Nothing
         return convert_to_time(value)
@@ -35,7 +35,7 @@ end
 
 function select_set!(superset)
     # select set
-    set = superset[1][1][1,:]
+    set = copy(superset[1][1][1,:])
 
     # clear weights of chosen set
     superset[1][1][1, :priority] = 0
